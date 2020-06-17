368a14b8d5abe21bf94b43d8db0e34d6cf183d05
Guilherme <guilhermeasrgl9@gmail.com>
2020-06-16 22:14:35 -0300

    First commit

---
+++ Assets/Scripts/CellController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using UnityEngine;
+
+public class CellController : MonoBehaviour
+{
+
+    private GameObject emptyCell;
+
+    public int correctPosition;
+
+    public bool isOnCorrectPosition;
+
+    void Start()
+    {
+        emptyCell = GameObject.FindGameObjectWithTag("Placeholder");
+    }
+
+    private void OnMouseDown()
+    {
+        if (CanMove())
+        {
+            Move();
+        }
+    }
+
+    private bool CanMove()
+    {
+        Vector2 emptyPosition = emptyCell.transform.localPosition;
+        Vector2 clickedPosition = transform.localPosition;
+
+        float horizontalDistance = Mathf.Abs(emptyPosition.x - clickedPosition.x);
+        float verticalDistance = Mathf.Abs(emptyPosition.y - clickedPosition.y);
+
+        if (horizontalDistance >= 2f || verticalDistance >= 2f)
+        {
+            return false;
+        }
+
+        bool canMoveHorizontal = horizontalDistance == 1F;
+        bool canMoveVertical = verticalDistance == 1F;
+
+        if (canMoveHorizontal && canMoveVertical)
+        {
+            return false;
+        }
+        
+        if (canMoveHorizontal || canMoveVertical)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    private void Move()
+    {
+        Vector2 currentPosition = transform.position;
+
+        transform.position = Vector2.Lerp(transform.position, emptyCell.transform.position, 10f);
+        emptyCell.transform.position = Vector2.Lerp(emptyCell.transform.position, currentPosition, 10f);
+
+        //Here we should update the isOnCorrectPosition variable based on the position (???)
+
+        FindObjectOfType<GameMaster>().CheckGameFinished();
+    }
+
+}
---
+++ Assets/Scripts/GameMaster.cs
@@ -0,0 +1,46 @@
+﻿using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameMaster : MonoBehaviour
+{
+
+    public Color[] colors = new Color[8];
+
+    public Text winText;
+
+    void Start()
+    {
+        winText.enabled = false;
+        FillGrid();
+        TurnEmptyInvisible();
+    }
+
+    private void FillGrid()
+    {
+        GameObject[] gm = GameObject.FindGameObjectsWithTag("Player").ToList().OrderBy(x => Random.value).ToArray();
+
+        for (int index = 0; index < gm.Length; index++)
+        {
+            gm[index].GetComponent<SpriteRenderer>().color = colors[index];
+            gm[index].GetComponent<CellController>().correctPosition = index;
+        }
+
+    }
+
+    private void TurnEmptyInvisible()
+    {
+        GameObject.FindGameObjectWithTag("Placeholder").GetComponent<SpriteRenderer>().color = new Color(0, 0, 0, 0);
+    }
+
+    public void CheckGameFinished()
+    {
+        GameObject[] gm = GameObject.FindGameObjectsWithTag("Player");
+
+        if (gm.All(cell => cell.GetComponent<CellController>().isOnCorrectPosition))
+        {
+            winText.enabled = true;
+        }
+    }
+
+}
